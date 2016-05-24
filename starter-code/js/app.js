5fc4a0695f6f8402d3c7ea330462bda89b62682b
kendallgoodwin <kendall@kendallgoodwin.com>
2016-05-24 15:12:38 -0700

    search is returning data

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,7 +1,23 @@
 var app = angular.module('RedditDashApp', []);
 app.controller('DashCtrl', ['$scope', '$http', function($scope, $http) {
 
-	$scope.running = "running"
+	$scope.searchTerm = '';
+
+	$scope.search = function() {
+		var req = {
+			url: 'http://www.reddit.com/search.json?q=' + $scope.searchTerm, 
+			method: 'get'
+		}
+		$http(req).then(function success(res) {
+			var redditData = res.data;
+			console.log(redditData);
+
+		}, function error(res) {
+			console.log(res);
+		});
+	}
+	
+
+
 }])
 
-console.log('this file is running.');
