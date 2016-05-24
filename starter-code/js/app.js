f145413714ce92000c56885682641c2cc725890a
kendallgoodwin <kendall@kendallgoodwin.com>
2016-05-24 15:00:12 -0700

    initial commit

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,3 +1,7 @@
-/* put js code here */
+var app = angular.module('RedditDashApp', []);
+app.controller('DashCtrl', ['$scope', '$http', function($scope, $http) {
+
+	$scope.running = "running"
+}])
 
 console.log('this file is running.');
