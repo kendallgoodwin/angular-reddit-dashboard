b8224e28c4b6ddc2d58152a0b749a2cdcab841da
kendallgoodwin <kendall@kendallgoodwin.com>
2016-05-24 19:28:53 -0700

    delete from history, search results from history

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,11 +1,12 @@
 var app = angular.module('RedditDashApp', []);
 app.controller('DashCtrl', ['$scope', '$http', function($scope, $http) {
+	
 	if(localStorage.getItem('hist')){
 		$scope.history = JSON.parse(localStorage.getItem("hist"));
 	}else{
 		$scope.history=[];
 	}
-	// $scope.items = JSON.parse("history")
+
 	$scope.searchTerm = '';
 	$scope.articleArray = [];
 
@@ -20,7 +21,7 @@ app.controller('DashCtrl', ['$scope', '$http', function($scope, $http) {
 			var redditData = res.data;
 			var articleData = redditData.data.children
 			// console.log(redditData);
-
+			$scope.articleArray = [];
 			for (var i = 0; i < articleData.length; i++) {
 				var articles = articleData[i];
 				// console.log(articles);
@@ -35,6 +36,41 @@ app.controller('DashCtrl', ['$scope', '$http', function($scope, $http) {
 		}, function error(res) {
 			console.log(res);
 		});
+	}
+
+	$scope.research = function(item) {
+		$scope.searchTerm = item;
+
+		var req = {
+			url: 'http://www.reddit.com/search.json?q=' + item, 
+			method: 'get'
+		}
+		$http(req).then(function success(res) {
+			var redditData = res.data;
+			var articleData = redditData.data.children
+			// console.log(redditData);
+			$scope.articleArray = [];
+			for (var i = 0; i < articleData.length; i++) {
+				var articles = articleData[i];
+				// console.log(articles);
+				$scope.articleArray.push(articles);
+				// console.log($scope.articleArray);
+			} 
+
+			$scope.history.push(item);
+			localStorage.setItem("hist", JSON.stringify($scope.history));
+			console.log(localStorage.getItem('hist'))
+
+		}, function error(res) {
+			console.log(res);
+		});
+
+		search();
+	}
+
+	$scope.remove = function(i) {
+		$scope.history.splice(i, 1);
+		localStorage.setItem("hist", JSON.stringify($scope.history));
 	}
 	
 
