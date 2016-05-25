e389ecfa22b4b1cbe9f810a88b02a064d71531e0
kendallgoodwin <kendall@kendallgoodwin.com>
2016-05-24 18:34:44 -0700

    saving search terms to localstorage and populating the sidebar with the history

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,13 +1,15 @@
 var app = angular.module('RedditDashApp', []);
 app.controller('DashCtrl', ['$scope', '$http', function($scope, $http) {
-
+	if(localStorage.getItem('hist')){
+		$scope.history = JSON.parse(localStorage.getItem("hist"));
+	}else{
+		$scope.history=[];
+	}
+	// $scope.items = JSON.parse("history")
 	$scope.searchTerm = '';
-	// $scope.title = '';
-	// $scope.img = '';
-	// $scope.score = 0;
-	// $scope.comments = 0;
-	// $scope.author = '';
-	$scope.articleArray = []
+	$scope.articleArray = [];
+
+	
 
 	$scope.search = function() {
 		var req = {
@@ -26,12 +28,9 @@ app.controller('DashCtrl', ['$scope', '$http', function($scope, $http) {
 				// console.log($scope.articleArray);
 			} 
 
-			// $scope.title = articleArray[0].data.title;
-			// $scope.img = articleData[0].data.thumbnail;
-			// $scope.score = articleData[0].data.score;
-			// $scope.comments = articleData[0].data.num_comments;
-			// $scope.author = articleData[0].data.author;
-
+			$scope.history.push($scope.searchTerm);
+			localStorage.setItem("hist", JSON.stringify($scope.history));
+			console.log(localStorage.getItem('hist'))
 
 		}, function error(res) {
 			console.log(res);
